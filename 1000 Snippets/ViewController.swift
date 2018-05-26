c4ad3cc3f6890636bd3cee8fd1ac431b7b7d561d
Luciano Sclovsky <lucianosclovsky@MacBook-Pro-de-Luciano.local>
2018-05-25 22:43:26 -0300

    1000 Snippets project kick off

---
+++ 1000 Snippets/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  1000 Snippets
+//
+//  Created by Luciano Sclovsky on 25/05/2018.
+//  Copyright © 2018 Luciano Sclovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstButton: UIButton!
+    
+    var snippet: [String: Any] = [:]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        snippet = [
+            "snippet": 1,
+            "title": "UIButton change text",
+            "links": [
+                "https://stackoverflow.com/questions/1033763/is-it-possible-to-update-uibutton-title-text-programmatically"
+            ]
+        ]
+        firstButton.setTitle("1000 Snippets", for: .normal)
+
+        snippet = [
+            "snippet": 2,
+            "title": "UIButton change font name",
+            "links": [
+                "https://stackoverflow.com/questions/25002017/how-to-change-font-of-uibutton-with-swift"
+            ]
+        ]
+        firstButton.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 17.0)
+        
+        // TODO: snippet for color
+        snippet = [
+            "snippet": 3,
+            "title": "UIButton change font color",
+            "links": [
+                "https://stackoverflow.com/questions/2474289/how-can-i-change-uibutton-title-color"
+            ]
+        ]
+        firstButton.setTitleColor(.red, for: .normal)
+
+        snippet = [
+            "snippet": 4,
+            "title": "UIButton change background color"
+        ]
+        firstButton.backgroundColor = .lightGray
+
+        
+    }
+
+}
+
