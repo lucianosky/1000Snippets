ddca81eb7c097129c908d3e101e13617add761f4
Luciano Sclovsky <luciano.sky@gmail.com>
2019-04-01 20:08:53 -0300

    Added snippets #7, #8 for UIButton

--- 1000 Snippets/ViewController.swift
+++ 1000 Snippets/ViewController.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
+// TODO - snippet at top level
+extension UIButton {
+    convenience init(_ title: String = "") {
+        self.init(type: .custom)
+        setTitle(title, for: .normal)
+        setTitleColor(.blue, for: .normal)
+        setTitleColor(.red, for: .selected)
+        // TODO color name
+        backgroundColor = UIColor.init(white: 0.9, alpha: 1.0)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
 class ViewController: UIViewController {
 
     let firstButton = UIButton()
     let secondButton = UIButton()
+    
+    let aButton = UIButton("a")
+    let bButton = UIButton("b")
+    let cButton = UIButton("c")
 
     var snippet: [String: Any] = [:]
+    
+    var currentButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,19 +45,25 @@ class ViewController: UIViewController {
         firstButton.translatesAutoresizingMaskIntoConstraints = false
         secondButton.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .white
-        view.addSubviews([firstButton, secondButton])
+        view.addSubviews([firstButton, secondButton, aButton, bButton, cButton])
     }
     
     func createConstraints() {
         let dict: [String: Any] = [
             "btn1": firstButton,
             "btn2": secondButton,
+            "aBtn": aButton,
+            "bBtn": bButton,
+            "cBtn": cButton,
             ]
-        activateConstraints("V:|-100-[btn1(50)]-20-[btn2(50)]", views: dict)
+        activateConstraints("V:|-100-[btn1(50)]-20-[btn2(50)]-20-[aBtn]", views: dict)
         activateConstraints("H:[btn1(150)]", views: dict)
+        activateConstraints("H:|-20-[aBtn(50)]-20-[bBtn]-20-[cBtn]", views: dict)
         firstButton.equalConstraints([.centerX], to: view)
         secondButton.equalConstraints([.centerX], to: view)
         secondButton.equalConstraints([.width], to: firstButton)
+        bButton.equalConstraints([.top, .width], to: aButton)
+        cButton.equalConstraints([.top, .width], to: aButton)
     }
     
     func createSnippets() {
@@ -102,14 +127,45 @@ class ViewController: UIViewController {
             ]
         ]
         secondButton.isEnabled = false
+
+        snippet = [
+            "id": 7,
+            "title": "UIButton set state selected",
+            "links": [
+                "https://stackoverflow.com/questions/25944791/how-to-change-the-state-of-an-uibutton-in-ios-using-swift"
+            ]
+        ]
+        aButton.isSelected = true
         
+        currentButton = aButton
+        
+        snippet = [
+            "id": 8,
+            "title": "UIButton add target action for touchUpInside",
+            "links": [
+                "https://stackoverflow.com/questions/24102191/make-a-uibutton-programmatically-in-swift"
+            ]
+        ]
+        aButton.addTarget(self, action:#selector(self.buttonPressed), for: .touchUpInside)
+
+        bButton.addTarget(self, action:#selector(self.buttonPressed), for: .touchUpInside)
+        cButton.addTarget(self, action:#selector(self.buttonPressed), for: .touchUpInside)
+
         // TODO:
-        // button isSelected
         // button - radio
         // UIColor extension, named color
         // UIButtonExtension, with corner radius, etc
-        
+        // revisar link do snippet 8: "https://stackoverflow.com/questions/24102191/make-a-uibutton-programmatically-in-swift"
+
     }
 
+    @objc func buttonPressed(sender: UIButton!) {
+        if currentButton != sender {
+            currentButton.isSelected = false
+            sender.isSelected = true
+            currentButton = sender
+        }
+    }
+    
 }
 
