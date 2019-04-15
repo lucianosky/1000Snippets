52ea8b943e2909cffb9cdc7604b2b4b94188c5d9
Luciano Sclovsky <luciano.sclovsky@softdesign-rs.com.br>
2019-04-14 21:06:37 -0300

    Created ButtonViewController to refactor code

---
+++ 1000 Snippets/ButtonViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonViewController.swift
+//  1000 Snippets
+//
+//  Created by Luciano Sclovsky on 14/04/19.
+//  Copyright © 2019 Luciano Sclovsky. All rights reserved.
+//
+
+import UIKit
+
+class ButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
